e2a3ab41e1596b247d5306227f42fc54e4c4121d
Lawybanx <awwalbankole@gmail.com>
2021-02-18 21:29:53 +0100

    Mern skeleton v1

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config({ path: './config/config.env' });
+
+// Connect MongoDB at default port 27017.
+connectDB();
+
+// Express Init
+const app = express();
+
+// Body-parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/~/~', require('./routes/~/~'));
+
+// Static Build Folder
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// PORT
+const PORT = process.env.PORT || 3000;
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  );
+});
